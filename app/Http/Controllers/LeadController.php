7113366bfc94d5893d9bc56775e79de1aa0fa82f
Christopher Hardy <chris.hardy1989@hotmail.com>
2023-03-04 22:36:47 -0500

    update controllers with auth middleware

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LeadController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Create a new lead
      * 
