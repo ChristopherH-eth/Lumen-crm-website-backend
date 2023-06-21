959f7eacc3ce76e6b9d52220ce82c3927c892f45
Christopher Hardy <chris.hardy1989@hotmail.com>
2023-06-20 22:56:03 -0400

    update Action Bar seeders

--- database/seeders/AccountsActionBarsTableSeeder.php
+++ database/seeders/AccountsActionBarsTableSeeder.php
@@ -15,17 +15,40 @@ class AccountsActionBarsTableSeeder extends Seeder
      */
     public function run()
     {
-        $actionBar = [
+        // Default action bar for Account collections
+        $defaultCollectionActionBar = [
             ['id' => 0, 'button' => 'new', 'text' => 'New', 'options' => ['hasMenu' => false]],
-            ['id' => 1, 'button' => 'view', 'text' => 'View', 'options' => ['hasMenu' => true]],
+            ['id' => 1, 'button' => 'view', 'text' => 'View', 'options' => ['hasMenu' => true],
+                'menu' => [
+                    ['id' => 0, 'text' => 'New'], 
+                    ['id' => 1, 'text' => 'Edit'], 
+                    ['id' => 2, 'text' => 'Manage']
+                ]
+            ],
             ['id' => 2, 'button' => 'import', 'text' => 'Import', 'options' => ['hasMenu' => false]],
             ['id' => 3, 'button' => 'export', 'text' => 'Export', 'options' => ['hasMenu' => false]],
-            ['id' => 4, 'button' => 'more', 'text' => 'More', 'options' => ['hasMenu' => true]]
+            ['id' => 4, 'button' => 'more', 'text' => 'More', 'options' => ['hasMenu' => true],
+                'menu' => [
+                    ['id' => 0, 'text' => 'More Items']
+                ]
+            ]
+        ];
+
+        // Default action bar for Account entries
+        $defaultEntryActionBar = [
+            ['id' => 0, 'button' => 'edit', 'text' => 'Edit', 'options' => ['hasMenu' => false]],
+            ['id' => 1, 'button' => 'delete', 'text' => 'Delete', 'options' => ['hasMenu' => false]]
         ];
 
         DB::table('accounts_action_bars')->insert([
-            'name' => 'default',
-            'action_bar_data' => json_encode($actionBar)
+            [
+                'name' => 'collection_default',
+                'action_bar_data' => json_encode($defaultCollectionActionBar)
+            ],
+            [
+                'name' => 'entry_default',
+                'action_bar_data' => json_encode($defaultEntryActionBar)
+            ]
         ]);
     }
 }
--- database/seeders/LeadsActionBarsTableSeeder.php
+++ database/seeders/LeadsActionBarsTableSeeder.php
@@ -15,17 +15,40 @@ class LeadsActionBarsTableSeeder extends Seeder
      */
     public function run()
     {
-        $actionBar = [
+        // Default action bar for Lead collections
+        $defaultCollectionActionBar = [
             ['id' => 0, 'button' => 'new', 'text' => 'New', 'options' => ['hasMenu' => false]],
-            ['id' => 1, 'button' => 'view', 'text' => 'View', 'options' => ['hasMenu' => true]],
+            ['id' => 1, 'button' => 'view', 'text' => 'View', 'options' => ['hasMenu' => true],
+                'menu' => [
+                    ['id' => 0, 'text' => 'New'], 
+                    ['id' => 1, 'text' => 'Edit'], 
+                    ['id' => 2, 'text' => 'Manage']
+                ]
+            ],
             ['id' => 2, 'button' => 'import', 'text' => 'Import', 'options' => ['hasMenu' => false]],
             ['id' => 3, 'button' => 'export', 'text' => 'Export', 'options' => ['hasMenu' => false]],
-            ['id' => 4, 'button' => 'more', 'text' => 'More', 'options' => ['hasMenu' => true]]
+            ['id' => 4, 'button' => 'more', 'text' => 'More', 'options' => ['hasMenu' => true],
+                'menu' => [
+                    ['id' => 0, 'text' => 'More Items']
+                ]
+            ]
+        ];
+
+        // Default action bar for Lead entries
+        $defaultEntryActionBar = [
+            ['id' => 0, 'button' => 'edit', 'text' => 'Edit', 'options' => ['hasMenu' => false]],
+            ['id' => 1, 'button' => 'delete', 'text' => 'Delete', 'options' => ['hasMenu' => false]]
         ];
 
         DB::table('leads_action_bars')->insert([
-            'name' => 'default',
-            'action_bar_data' => json_encode($actionBar)
+            [
+                'name' => 'collection_default',
+                'action_bar_data' => json_encode($defaultCollectionActionBar)
+            ],
+            [
+                'name' => 'entry_default',
+                'action_bar_data' => json_encode($defaultEntryActionBar)
+            ]
         ]);
     }
 }
